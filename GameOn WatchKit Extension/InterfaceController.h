47a4f6ca63e7d351ac6d4d60312c1bebd9dc9611
Adam Groom <adam.groom@gmail.com>
2014-11-21 21:37:23 +0000

    Using different storyboard as workaround

---
+++ GameOn WatchKit Extension/InterfaceController.h
@@ -0,0 +1,15 @@
+//
+//  InterfaceController.h
+//  GameOn WatchKit Extension
+//
+//  Created by Adam Groom on 21/11/2014.
+//  Copyright (c) 2014 Team Awesome. All rights reserved.
+//
+
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
