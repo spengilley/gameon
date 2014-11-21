742ae2622d4ff9dc01c0f81729b52e384937f16b
spengilley <spengilley@gmail.com>
2014-11-21 16:17:43 +0000

    added image to top of feedviewcontroller

--- FeedViewController.m
+++ FeedViewController.m
@@ -29,6 +29,13 @@
     
     self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:50.0/255.0 green:50.0/255.0 blue:50.0/255.0 alpha:1.0];
     self.navigationController.navigationBar.translucent = NO;
+    
+    UIImage *image = [UIImage imageNamed:@"logo"];
+    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:image];
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:50.0/255.0 green:50.0/255.0 blue:50.0/255.0 alpha:1.0];
+    self.navigationController.navigationBar.translucent = NO;
+
 }
 
 
--- Game.m
+++ Game.m
@@ -52,11 +52,14 @@ NSString *const endKey = @"end key";
         self.theirScore = [[NSNumber alloc] initWithInt:arc4random_uniform(3)];
         
         NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        //self.start = [dateFormatter ]
+        self.start = [dateFormatter dateFromString:@"21/11/2014 13:00"];
+        self.end = [dateFormatter dateFromString:@"21/11/2014 13:25"];
     } else {
         self.gameID = gameOnDisk.gameID;
-        
-        
+        self.myScore = gameOnDisk.myScore;
+        self.theirScore = gameOnDisk.theirScore;
+        self.start = gameOnDisk.start;
+        self.end = gameOnDisk.end;
     }
 }
 
@@ -64,7 +67,7 @@ NSString *const endKey = @"end key";
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *filePath = [NSString stringWithFormat:@"%@/%@", documentsDirectory, @"me.txt"];
+    NSString *filePath = [NSString stringWithFormat:@"%@/%@", documentsDirectory, @"game.txt"];
     [NSKeyedArchiver archiveRootObject:self toFile:filePath];
 }
 
