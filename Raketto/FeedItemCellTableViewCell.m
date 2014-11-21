31d3632b3356dab5bad324c396ebb4d92d515a7d
spengilley <spengilley@gmail.com>
2014-11-21 12:05:59 +0000

    added tableview and tableviewcell class stubs

---
+++ Raketto/FeedItemCellTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  FeedItemCellTableViewCell.m
+//  Raketto
+//
+//  Created by Stephen Pengilley on 21/11/2014.
+//  Copyright (c) 2014 Team Awesome. All rights reserved.
+//
+
+#import "FeedItemCellTableViewCell.h"
+
+@implementation FeedItemCellTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
