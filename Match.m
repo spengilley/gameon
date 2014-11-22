b24025a0b4e93d0569a3a7e829017298cb7bfdc5
spengilley <spengilley@gmail.com>
2014-11-22 11:00:28 +0000

    corrected save and ubildfromfize steps for Match model

--- Match.m
+++ Match.m
@@ -24,7 +24,6 @@ NSString *const gamesKey = @"games key";
 }
 
 
-
 - (void)encodeWithCoder:(NSCoder *)aCoder{
     [aCoder encodeObject:self.matchId forKey:matchIDKey];
     [aCoder encodeObject:self.myUser forKey:myUserKey];
@@ -52,34 +51,34 @@ NSString *const gamesKey = @"games key";
 -(void)buildFromFile
 {
     NSURL *baseUrl = [[[NSFileManager alloc] init] containerURLForSecurityApplicationGroupIdentifier:@"group.adam"];
-    NSURL *url = [NSURL URLWithString:@"current_match" relativeToURL:baseUrl];
+    NSURL *url = [NSURL URLWithString:self.matchId relativeToURL:baseUrl];
     NSString *filePath = [url absoluteString];
     
-//    Game *gameOnDisk =
-    [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
+    Match *matchOnDisk = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
     
-//    if (!gameOnDisk) {
-//        self.gameID = [[NSNumber alloc] initWithInt:0];
-//        self.myScore = [[NSNumber alloc] initWithInt:arc4random_uniform(3)];
-//        self.theirScore = [[NSNumber alloc] initWithInt:arc4random_uniform(3)];
-//        
-//        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-//        self.start = [dateFormatter dateFromString:@"21/11/2014 13:00"];
-//        self.end = [dateFormatter dateFromString:@"21/11/2014 13:25"];
-//    } else {
-//        self.gameID = gameOnDisk.gameID;
-//        self.myScore = gameOnDisk.myScore;
-//        self.theirScore = gameOnDisk.theirScore;
-//        self.start = gameOnDisk.start;
-//        self.end = gameOnDisk.end;
-//    }
+    if(!matchOnDisk) {
+        self.matchId = [[NSUUID UUID] UUIDString];
+        self.myUser = [[User alloc] init];
+        [self.myUser buildFromFile];
+        self.theirUser = [[User alloc]init];
+        [self.theirUser buildFromFile];
+        self.matchStart = [[NSDate alloc] init];
+        self.matchEnd = [[NSDate alloc] init];
+        
+    } else {
+        self.matchId = matchOnDisk.matchId;
+        self.myUser = matchOnDisk.myUser;
+        self.theirUser = matchOnDisk.theirUser;
+        self.matchStart = [[NSDate alloc] init];
+        self.matchEnd = [[NSDate alloc] init];
+    }
 }
 
 -(void)save
 {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *filePath = [NSString stringWithFormat:@"%@/%@", documentsDirectory, @"game.txt"];
+    NSURL *baseUrl = [[[NSFileManager alloc] init] containerURLForSecurityApplicationGroupIdentifier:@"group.adam"];
+    NSURL *url = [NSURL URLWithString:self.matchId relativeToURL:baseUrl];
+    NSString *filePath = [url absoluteString];
     [NSKeyedArchiver archiveRootObject:self toFile:filePath];
 }
 
