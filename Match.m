2f99ca7882d25004056645ac0ebfdb9617577e4d
Adam Groom <adam.groom@gmail.com>
2014-11-22 10:44:27 +0000

    Small changes

--- Match.m
+++ Match.m
@@ -50,21 +50,21 @@ NSString *const gamesKey = @"games key";
     Game *gameOnDisk =
     [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
     
-    if (!gameOnDisk) {
-        self.gameID = [[NSNumber alloc] initWithInt:0];
-        self.myScore = [[NSNumber alloc] initWithInt:arc4random_uniform(3)];
-        self.theirScore = [[NSNumber alloc] initWithInt:arc4random_uniform(3)];
-        
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        self.start = [dateFormatter dateFromString:@"21/11/2014 13:00"];
-        self.end = [dateFormatter dateFromString:@"21/11/2014 13:25"];
-    } else {
-        self.gameID = gameOnDisk.gameID;
-        self.myScore = gameOnDisk.myScore;
-        self.theirScore = gameOnDisk.theirScore;
-        self.start = gameOnDisk.start;
-        self.end = gameOnDisk.end;
-    }
+//    if (!gameOnDisk) {
+//        self.gameID = [[NSNumber alloc] initWithInt:0];
+//        self.myScore = [[NSNumber alloc] initWithInt:arc4random_uniform(3)];
+//        self.theirScore = [[NSNumber alloc] initWithInt:arc4random_uniform(3)];
+//        
+//        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+//        self.start = [dateFormatter dateFromString:@"21/11/2014 13:00"];
+//        self.end = [dateFormatter dateFromString:@"21/11/2014 13:25"];
+//    } else {
+//        self.gameID = gameOnDisk.gameID;
+//        self.myScore = gameOnDisk.myScore;
+//        self.theirScore = gameOnDisk.theirScore;
+//        self.start = gameOnDisk.start;
+//        self.end = gameOnDisk.end;
+//    }
 }
 
 -(void)save
